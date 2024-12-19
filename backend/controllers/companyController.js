a604ddf4c1107717cd8c9eacd3ea8be59955e5a8
ritik02122002 <ritikgoyal4019@gmail.com>
2024-12-19 23:14:38 +0530

    added controller to Update Company details

--- backend/controllers/companyController.js
+++ backend/controllers/companyController.js
@@ -1,5 +1,8 @@
 import Company from "../models/CompanyModel.js";
-import { VALID_COMPANY_DETAILS } from "../utils/constant.js";
+import {
+  VALID_COMPANY_DETAILS,
+  VALID_COMPANY_UPDATES,
+} from "../utils/constant.js";
 
 export const registerCompany = async (req, res) => {
   try {
@@ -8,8 +11,11 @@ export const registerCompany = async (req, res) => {
       VALID_COMPANY_DETAILS.includes(detail)
     );
     if (!areDetailsValid) throw new Error("invalid details");
-    if (!companyDetails.website) throw new Error("missing company website");
-    const company = await Company.findOne({ website: companyDetails.website });
+    if (!companyDetails.companyDomain)
+      throw new Error("missing company domain");
+    const company = await Company.findOne({
+      companyDomain: companyDetails.companyDomain,
+    });
     if (company) throw new Error("company already exists");
     const comp = new Company(companyDetails);
     await comp.save();
@@ -34,6 +40,43 @@ export const getCompanyDetails = async (req, res) => {
     if (!company) throw new Error("Comapny id is invalid");
     res.json({
       message: `${company.name}'s detail fetched successfully `,
+      status: "success",
+      data: company,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "failure",
+      message: err.message,
+    });
+  }
+};
+
+export const updateCompanyDetails = async (req, res) => {
+  try {
+    const { user } = req;
+    const { companyId, ...companyDetails } = req.body;
+    if (!companyId) throw new Error("company id is missing");
+
+    const isUpdateValid = Object.keys(companyDetails).every((detail) =>
+      VALID_COMPANY_UPDATES.includes(detail)
+    );
+    if (!isUpdateValid) throw new Error("invalid company details");
+
+    let company = await Company.findById(companyId);
+    if (!company) throw new Error("company does not exist");
+    const companiesList = user.companyDetails;
+    const isCompanyEmailVerified = companiesList.some((comp) =>
+      comp.companyId.equals(companyId)
+    );
+    if (!isCompanyEmailVerified)
+      throw new Error("recruiter does not belong to company");
+
+    Object.keys(companyDetails).forEach((detail) => {
+      company[detail] = companyDetails[detail];
+    });
+    await company.save();
+    res.json({
+      message: `${company.name}'s detail updated successfully `,
       status: "success",
       data: company,
     });
